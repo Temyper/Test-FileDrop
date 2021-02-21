3abba504907bb8f6af7a1072feda2cb76a8ab99e
Temyper <pall47g1@gmail.com>
2021-02-21 15:05:20 +0900

    [Add] getting dropped files.

--- js/body.js
+++ js/body.js
@@ -1,10 +1,37 @@
+"use strict";
+
+const $droppingArea = $("#file-dropping__area");
+
+const $target = $("#file-dropping__target");
+
+const droppingColor = "#ff0000";
+
+const defaultColor = "";
+
 $(function AddEvents() {
-  $("#file-dropping__area").on("dragover", function () {
-    $("#file-dropping__target").css("background-color", "#ff0000");
-    event.preventDefault();
+  $droppingArea.on("dragover", function (e) {
+    $target.css("background-color", droppingColor);
+    console.log("dragover");
+    e.preventDefault();
   });
-  $("#file-dropping__area").on("dragleave", function () {
-    $("#file-dropping__target").css("background-color", "rgb(96, 235, 96)");
-    event.preventDefault();
+  $droppingArea.on("dragleave", function (e) {
+    $target.css("background-color", defaultColor);
+    console.log("dragleave");
+    e.preventDefault();
+  });
+
+  $droppingArea.on("drop", function (e) {
+    // 20210221最初にデフォルトのイベントを中止することで、ブラウザでドロップしたファイルを開くことを防ぐ
+    e.preventDefault();
+    $target.css("background-color", defaultColor);
+
+    // 20210221jQueryでただ置き換えると、本来のイベントが動かない（eにdataTransferプロパティが存在しない）
+    // if (e.dataTransfer.files.length > 0)
+    //   console.log(e.dataTransfer.files.length.toString());
+
+    let files = e.originalEvent.dataTransfer.files;
+    console.log(files.length.toString());
+
+    console.log("drop");
   });
 });
